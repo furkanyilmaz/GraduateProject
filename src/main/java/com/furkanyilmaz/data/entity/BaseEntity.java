1f1ee2661b4c11710d7e0bdc8e26ffd008a13cef
furkanyilmaz <furkanyilmaz-@hotmail.com>
2022-10-31 22:14:51 +0300

    Auditing entity

--- src/main/java/com/furkanyilmaz/data/entity/BaseEntity.java
+++ src/main/java/com/furkanyilmaz/data/entity/BaseEntity.java
@@ -3,20 +3,48 @@ package com.furkanyilmaz.data.entity;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
 import java.util.Date;
 //lombok
 @Getter @Setter
-@MappedSuperclass //extend edip bunu kullanmalıyız ki base olduğu anlaşılsın.
-public class BaseEntity {
+@MappedSuperclass //baseClass
+//auditing
+@EntityListeners(AuditingEntityListener.class)
+abstract public class BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
-    @Column(name = "created_date")
+    @Column(name = "system_created_date")
     @Temporal(TemporalType.TIMESTAMP)
     @CreationTimestamp
+    private Date systemCreatedDate;
+
+    //auditing
+    //kim ekledi
+    @Column(name = "created_by")
+    @CreatedBy
+    private String createdBy;
+
+    //kim ne zaman ekledi
+    @Column(name = "created_date")
+    @CreatedDate
     private Date createdDate;
+
+    //kim güncelledi
+    @Column(name = "Last_Modified_By")
+    @LastModifiedBy
+    private String LastModifiedBy;
+
+    //kim ne zaman güncelledi
+    @Column(name = "LastModifiedDate")
+    @LastModifiedDate
+    private Date LastModifiedDate;
 }
