f6bdecfaf176e7a07364c35057ced3b1f695c647
furkanyilmaz <furkanyilmaz-@hotmail.com>
2022-11-01 22:05:05 +0300

    initial register commit

---
+++ src/main/java/com/furkanyilmaz/GraduateProjectRegisterLoginApplication.java
@@ -0,0 +1,13 @@
+package com.furkanyilmaz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraduateProjectRegisterLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraduateProjectRegisterLoginApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/furkanyilmaz/GraduateProjectRegisterLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.furkanyilmaz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraduateProjectRegisterLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
