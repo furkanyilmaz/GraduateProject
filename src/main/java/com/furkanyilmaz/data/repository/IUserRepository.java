5728460eebfcf49f382323276a0cb055a5b12983
furkanyilmaz <furkanyilmaz-@hotmail.com>
2022-11-01 23:07:17 +0300

    User dto, entity, repository

---
+++ src/main/java/com/furkanyilmaz/data/repository/IUserRepository.java
@@ -0,0 +1,14 @@
+package com.furkanyilmaz.data.repository;
+
+import com.furkanyilmaz.data.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface IUserRepository extends JpaRepository<UserEntity,Long> {
+
+    //delivered query
+    Optional<UserEntity> findByUsername(String username);
+}
