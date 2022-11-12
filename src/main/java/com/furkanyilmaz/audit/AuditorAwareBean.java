7113fa83d5ba1186e0e188d0c57bff6c059fa909
furkanyilmaz <furkanyilmaz-@hotmail.com>
2022-11-12 13:59:50 +0300

    enable JPA Auditing

--- src/main/java/com/furkanyilmaz/audit/AuditorAwareBean.java
+++ src/main/java/com/furkanyilmaz/audit/AuditorAwareBean.java
@@ -3,8 +3,10 @@ package com.furkanyilmaz.audit;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @Configuration
+@EnableJpaAuditing
 public class AuditorAwareBean {
 
     @Bean
