9b2a603294bb13f661a3f8e093f971ae44a2c28a
furkanyilmaz <furkanyilmaz-@hotmail.com>
2022-11-20 04:27:31 +0300

    last update

--- src/main/java/com/furkanyilmaz/ui/api/Impl/AuthenticationApiImpl.java
+++ src/main/java/com/furkanyilmaz/ui/api/Impl/AuthenticationApiImpl.java
@@ -3,10 +3,12 @@ package com.furkanyilmaz.ui.api.Impl;
 import com.furkanyilmaz.business.dto.UserDto;
 import com.furkanyilmaz.business.services.IAuthenticationService;
 import com.furkanyilmaz.business.services.IUserServices;
+import com.furkanyilmaz.security.jwt.JwtProviderImpl;
 import com.furkanyilmaz.ui.api.IAuthenticationApi;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,6 +24,10 @@ public class AuthenticationApiImpl implements IAuthenticationApi {
     //Injection
     private final IAuthenticationService authenticationService;
     private final IUserServices userServices;
+
+
+    private final AuthenticationManager authenticationManager;
+    private final JwtProviderImpl jwtUtils;
 
 
     //REGISTER
